ff18d43cecd2788daf4c3b5f60cd1e396a8ca043
JesusRibon <ribonjesus123@gmail.com>
2023-04-14 10:51:33 -0500

    Filtro por id Cliente

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -8,11 +8,21 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, ...arg: any[]): any {
     const resultado=[];
     for(const persona of value) {
-      if(persona.id.toLowerCase().indexOf(arg)>-1){
+      if(persona.id.toLowerCase().indexOf(arg)>-1 ){
         resultado.push(persona);
       };
+    
     };
+    
+
+
     return resultado;
+
+    
   }
+
+
+
+
 
 }
